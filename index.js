e210e96d3e544e7a3766afcb8f9c84975afafaa2
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-10-28 10:02:26 +0700

    add nodemon on package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
-const port = 3000;
+const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
