50fd3e7d069c9798efcda9a16b07fadf55a6570f
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-10-28 10:36:37 +0700

    add CRUD api

--- index.js
+++ index.js
@@ -19,3 +19,54 @@ db.sequelize
   .catch((err) => {
     console.log(err);
   });
+
+app.post("/komiks", async (req, res) => {
+  try {
+    const komik = await db.Komik.create(data);
+    res.send(komik);
+  } catch (error) {
+    res.send(err);
+  }
+});
+
+app.get("/komiks", async (req, res) => {
+  try {
+    const komiks = await db.Komik.findAll();
+    res.send(komiks);
+  } catch (error) {
+    res.send(err);
+  }
+});
+
+app.put("/komik/:id", async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const Komik = await db.Komik.findByPk(id);
+        it(!Komik) {
+            return res.status(404).send({ message: "Komik not found" });
+        }
+
+        await Komik.update(data);
+        res.send({message: "Komik updated successfully", Komik});
+    } catch (error) {
+        res.status(500).send(err);
+    }
+});
+
+app.delete("komik/:id", async(req, res) => {
+    const id = req.params.id;
+
+    try {
+        const Komik = await db.Komik.findByPk(id);
+        if(!Komik) {
+            return res.status(404).send({ message: "Komik not found" });
+        }
+
+        await Komik.destroy();
+        res.send({ message: "Komik deleted successfully" });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
