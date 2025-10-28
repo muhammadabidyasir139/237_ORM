74042e3f29733d32c810c0b1ccee33ba3f3affcc
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-10-28 09:56:28 +0700

    sett index js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const db = require("./models");
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize
+  .sync()
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
